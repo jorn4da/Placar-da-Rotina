4bd73cb0c0dc9b20da648d34a196a4285eab747b
Aritan Nakia Moreira Cordeiro <aritan@MacBook-Air-de-Aritan.local>
2026-09-12 19:23:34 -0300

    Atualiza Placar da Rotina (12/09/2026 19:23)

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE="placar-rotina-v8";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"];
+self.addEventListener("install",e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener("activate",e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(resp=>{
+    try{ const c=resp.clone(); caches.open(CACHE).then(cc=>cc.put(e.request,c)); }catch(_){}
+    return resp;
+  }).catch(()=> caches.match("./index.html"))));
+});
