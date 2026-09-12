f7f7cd468be9b93c616f8f91c201ed60cbfd7109
Aritan Nakia Moreira Cordeiro <aritan@MacBook-Air-de-Aritan.local>
2026-09-12 20:44:24 -0300

    Atualiza Placar da Rotina (12/09/2026 20:44)

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE="placar-rotina-v8";
+const CACHE="placar-rotina-v9";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"];
 self.addEventListener("install",e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener("activate",e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
+  var url=new URL(e.request.url);
+  if(url.origin!==location.origin) return; // deixa Firebase e CDNs passarem direto
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(resp=>{
     try{ const c=resp.clone(); caches.open(CACHE).then(cc=>cc.put(e.request,c)); }catch(_){}
     return resp;
